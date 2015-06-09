62661bdf3476191be8e65075595d132bb4bfbcce
Emil Apelgren <emil.apelgren@xlent.se>
2015-06-09 10:07:20 +0200

    Xamarin Init
    Xamarin Init Projetct

---
+++ XamarinAzureAD/XamarinAzureAD/XamarinAzureAD.Droid/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace XamarinAzureAD.Droid
+{
+    [Activity(Label = "XamarinAzureAD", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
+

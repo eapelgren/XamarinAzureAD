784c2a00dfc705feb2347c08a9d17e31b5f6d024
Emil Apelgren <emilapelgren@outlook.com>
2015-07-13 14:49:57 +0200

    updating for new pcl shared DTO model

---
+++ XamarinAzureAD/XamarinAzureADTest/UnitTest4.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace XamarinAzureADTest
+{
+    [TestClass]
+    public class UnitTest4
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+
+        }
+    }
+}
